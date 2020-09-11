53ef4b2b63eb133e897c9f187fbaadbfe405d059
Areg Karakhanyan <70661831+gyabaj@users.noreply.github.com>
2020-09-11 22:07:16 +0400

    Update sudoku.h

--- sudoku.h
+++ sudoku.h
@@ -1,19 +1,15 @@
 #pragma once
 #include <iostream>
 
-
-
 class Sudoku
 {
 private:
         int arr[9][9];
-
 public:
         Sudoku();
         Sudoku(int *arr, int n);
         Sudoku(const Sudoku &Sud);
         ~Sudoku();
-
         void enterDigits();
         bool solve();
         void print();
